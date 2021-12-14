0b9cade971b137978bd138cad4018ca5803a8d45
asuraaxu <asuraaxu@tencent.com>
2021-12-14 19:38:35 +0800

    release v1

--- server/consumers.py
+++ server/consumers.py
@@ -26,25 +26,26 @@ class UserConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        print('aaa:',message)
 
+        # await self.send(text_data=json.dumps({
+        #     'message': message
+        # }))
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.user_id_group,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message
         }))
-
-        # # Send message to room group
-        # await self.channel_layer.group_send(
-        #     self.user_id_group,
-        #     {
-        #         'type': 'chat_message',
-        #         'message': message
-        #     }
-        # )
-
-    # # Receive message from room group
-    # async def chat_message(self, event):
-    #     message = event['message']
-    #
-    #     # Send message to WebSocket
-    #     await self.send(text_data=json.dumps({
-    #         'message': message
-    #     }))
