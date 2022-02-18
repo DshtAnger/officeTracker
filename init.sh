c37a1bdfb92095f8ebcdbbe5933fc3dd38878934
asuraaxu <asuraaxu@tencent.com>
2022-02-18 14:58:29 +0800

    modify init file and add send email module

--- init.sh
+++ init.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-# 手工添加环境变量,~/.profile,/etc/apache2/envvars
+# 手工添加环境变量,~/.bashrc,/etc/apache2/envvars
+source /root/.bashrc
+source /etc/apache2/envvars
+echo '[+] Enable Computing Resource Envs Done.'
+
+sh ./install_requirements.sh
+echo '[+] Install OS And Python Requirements Done.'
 
 mkdir ./upload
 mkdir ./download
-
-sh ./install_requirements.sh
-
-sed -i "s/DEBUG = True/DEBUG = False/g" ./officeTracker/settings.py
-
-python3 ./manage.py makemigrations
-python3 ./manage.py migrate
-
 chmod 755 -R ../
 chown www-data:www-data -R ./upload/
 chown www-data:www-data -R ./download/
+echo '[+] Create Upload Download Folder And Configure Folder permissions Done.'
 
+sed -i "s/DEBUG = True/DEBUG = False/g" ./officeTracker/settings.py
+echo '[+] Close Project DEBUG Switch Done.'
+
+python3 ./manage.py makemigrations
+python3 ./manage.py migrate
+echo '[+] Initialize Project Database Done.'
 
 #locale-gen zh_CN.utf8
 #cp /etc/apache2/envvars /etc/apache2/envvars.old
@@ -24,19 +29,20 @@ chown www-data:www-data -R ./download/
 #echo "export LANG=zh_CN.utf8" >> /etc/apache2/envvars
 #source /etc/apache2/envvars
 
-
 sed -i "s/ServerName XXX/ServerName ${HOST_SERVER}/g" ./officeTracker.conf
 cp ./officeTracker.conf /etc/apache2/sites-available
+echo '[+] Configure Project Apache Conf File Done.'
 
 a2enmod proxy
 a2enmod proxy_http
 a2enmod proxy_wstunnel
+echo '[+] Enable Apache Proxy Module Done.'
 
 a2dissite officeTracker
 service apache2 reload
 a2ensite officeTracker
 service apache2 restart
-
+echo '[+] Start Project Apache Server Done.'
 
 
 check_upload_server=`ps aux | grep "http.server --bind 0.0.0.0" | grep -v grep`
@@ -44,16 +50,18 @@ check_ws_server=`ps aux | grep "daphne officeTracker.asgi:application -b 0.0.0.0
 
 if  [[ $check_upload_server =~ "http" ]]
 then
-  echo "upload_server had started."
+  echo "[*] Upload Server Had Started."
 else
   nohup python3 -u -m http.server --bind 0.0.0.0 8080 --directory /root/officeTracker/upload/ >> /root/officeTracker/upload.log 2>&1 &
-  echo "upload_server start Done."
+  echo "[+] Upload Server Starts Done."
 fi
 
 if [[ $check_ws_server =~ "daphne" ]]
 then
-  echo "ws_server had started."
+  echo "[*] Websocket Server Had Started."
 else
   nohup python3 -u /usr/local/bin/daphne officeTracker.asgi:application -b 0.0.0.0 -p 8888 >> /root/officeTracker/ws.log 2>&1 &
-  echo "ws_server start Done."
+  echo "[+] Websocket Server Starts Done."
 fi
+
+echo '[+] ALL Configurations Done.'
---
+++ send_email.py
@@ -0,0 +1,112 @@
+# coding=utf-8
+import asyncio
+import hashlib
+from aiohttp import ClientSession
+from aredis import StrictRedis
+import time
+import os
+import sys
+import random
+import json
+import logging
+import traceback
+
+
+# 办公机上访问OA区、IDC皆可,云梯申请到的机器位于IDC生产环境、只能访问IDC区
+paasId = os.getenv('paasId')
+paasToken = os.getenv('paasToken')
+server = "http://idc.rio.tencent.com"
+path = "/ebus/tof4_msg/api/v1/Message/SendMailInfo"
+
+
+logging.basicConfig(format='%(message)s',filename = f'/root/email.log', level=logging.INFO)
+def handle_exception(exc_type, exc_value, exc_traceback):
+	if issubclass(exc_type, KeyboardInterrupt):
+		sys.__excepthook__(exc_type, exc_value, exc_traceback)
+		return
+	logging.info("--------------------Uncaught Exception--------------------",exc_info=(exc_type, exc_value, exc_traceback))
+sys.excepthook = handle_exception
+
+def get_current_time():
+	return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+def gen_header():
+
+	timestamp = str(int(time.time()))
+	nonce = str(random.randint(1000, 9999))
+	signature = hashlib.sha256( (timestamp + paasToken + nonce + timestamp).encode() ).hexdigest().upper()
+
+	header = {}
+	header['x-rio-paasid'] = paasId
+	header['x-rio-nonce'] = nonce
+	header['x-rio-timestamp'] = timestamp
+	header['x-rio-signature'] = signature
+
+	return header
+
+redis = StrictRedis(host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT'), password=os.getenv('REDIS_PASSWORD'))
+EMAIL_QUEUE = 'email_task'
+
+async def send_email(user_id, upload_time, file_name, file_owner, access_time, access_ip, access_UA):
+	try:
+
+		data = {
+			"From": "YD-OfficeTracker@tencent.com",
+			"To": "%s@tencent.com"%user_id,
+			"CC": file_owner,
+			"Bcc": "",
+			"Title": "【警告】您的文档已被非法访问",
+
+			"Content":'''
+			<h2>Dear %s:</h2>
+			<h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;您于%s在平台上传的文档"%s"已被公司外的非法地址访问，具体非法访问信息如下:</h3>
+			<h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;访问时间:%s</h3>
+			<h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;访问地址:%s</h3>
+			<h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;访问指纹:"%s"</h3>
+			'''%(user_id,upload_time, file_name, access_time, access_ip, access_UA),
+
+			"EmailType": 1, 	#MAIL_TX = 1,     MAIL_INTERNET = 0. 收件人包含内外部的，需要将EmailType设置为0
+			"BodyFormat": 1,  	#HTML_FORMAT = 1, TEXT_FORMAT = 0
+			"Priority": 0
+		}
+
+		async with ClientSession() as session:
+			async with session.post(server + path, headers=gen_header(), data=json.dumps(data)) as response:
+				if response.status == 200:
+					logging.info(f'[+][{get_current_time()}][{user_id}] {file_name} was accessed illegally at {access_time} by {access_ip} with UA:{access_UA}.')
+				else:
+					logging.info(f'[*][{get_current_time()}][{user_id}] send email error with text: {response.text}')
+	except Exception:
+		exception_info = traceback.format_exc()
+		logging.info(exception_info)
+
+
+async def run():
+
+	while 1:
+
+		if not await redis.exists(EMAIL_QUEUE):
+			pass
+		else:
+			# 下发任务左插,取任务右取
+			data = await redis.rpop(EMAIL_QUEUE)
+			data = json.loads(data.decode('utf-8'))
+
+			user_id = data.get('user_id')
+			upload_time = data.get('upload_time')
+			file_name = data.get('file_name')
+			file_owner = data.get('file_owner')
+			access_time = data.get('access_time')
+			access_ip = data.get('access_ip')
+			access_UA = data.get('access_UA')
+			await send_email(user_id, upload_time, file_name, file_owner, access_time, access_ip, access_UA)
+
+			logging.info('-' * 50)
+
+		await asyncio.sleep(0.2)
+
+logging.info('Start send_emali...')
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+loop.run_until_complete(run())
