2531313a8a4173d7f22c701b2ccf294643524ac3
asuraaxu <asuraaxu@tencent.com>
2022-02-18 16:56:12 +0800

    modify email view nofity

--- server/views.py
+++ server/views.py
@@ -483,16 +483,6 @@ def track(request,file_watermark):
                 redis.hmset(f'{file_watermark}[{access_time.strftime("%Y%m%d%H%M%S")}]', {'times':'1'})
                 update_time = access_time
                 TO_NOTIFY = True
-
-                if not access_legitimacy:
-                    # 向email_task队列下发邮件通知任务
-                    email_data = {'user_id': file_obj.file_sharer, 'file_name': file_obj.file_name, 'file_owner':file_obj.user_id if file_obj.user_id!=file_obj.file_sharer else '',
-                                  'upload_time': timezone_to_string(file_obj.upload_time),
-                                  'access_time': timezone_to_string(access_time), 'access_ip': access_ip,
-                                  'access_UA': access_UA}
-                    redis.lpush('email_task', json.dumps(email_data))
-
-
             else:
                 # 访问间隔默认值2s以内，且本次访问缓存还没有刷新2次，则更新访问记录、并刷新缓存
                 if redis.hget(f'{file_watermark}[{lastest_access_list[0].access_time.strftime("%Y%m%d%H%M%S")}]','times').decode('utf-8') != '2':
@@ -512,6 +502,15 @@ def track(request,file_watermark):
                 if file_obj.user_id != file_obj.file_sharer:
                     task_data.update({'user_id': file_obj.user_id})
                     redis.lpush('track_task', json.dumps(task_data))
+
+        if not access_legitimacy:
+            # 向email_task队列下发邮件通知任务
+            email_data = {'user_id': file_obj.file_sharer, 'file_name': file_obj.file_name,
+                          'file_owner': file_obj.user_id if file_obj.user_id != file_obj.file_sharer else '',
+                          'upload_time': timezone_to_string(file_obj.upload_time),
+                          'access_time': timezone_to_string(access_time), 'access_ip': access_ip,
+                          'access_UA': access_UA}
+            redis.lpush('email_task', json.dumps(email_data))
 
         # win10打开doc,无论如何都只有一次访问记录.借助redis记录访问次数,当访问文档是doc时暂停2s再检测redis记录,如为达2则确定时win10打开doc场景,强制赋值并通知.
         # 判定access_UA是否为空的逻辑也可以整体修改为依靠redis的次数记录来判定
